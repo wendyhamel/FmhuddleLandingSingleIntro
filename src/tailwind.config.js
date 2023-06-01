a54b77c27eba858fc68c987cd8b6b65534c09a96
wendyhamel <whamel@it4web.nl>
2023-06-01 13:35:31 +0200

    Mobile styling

---
+++ src/tailwind.config.js
@@ -0,0 +1,14 @@
+tailwind.config = {
+	theme: {
+		fontFamily: {
+			'sans': ['Open Sans', 'sans-serif'],
+			'heading': ['Poppins', 'sans-serif']
+		},
+		extend: {
+			colors: {
+				'violet': 'hsl(257, 40%, 49%)',
+				'magenta': 'hsl(300, 69%, 71%)'
+			}
+		}
+	}
+}
